255fdb438b009f61c98d37b60d60780ba01db04c
samuel <olumidesamuel1914@gmail.com>
2021-12-02 13:41:43 +0100

    f

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -3,27 +3,46 @@ function Navbar(){
     return(
 
         <div className="navbar">
-              <div className='col-9'>
-          <div className="brand-box">
-              <a href='navbar-brand'> Dantata </a>
-          </div>
-          </div>
-          <div className='col-9'>
-          <div className='collapsible-nav'>
-              <ul>
-                  <li>
-                      Solar panels
-                  </li>
-                  <li>
-                      Solar inverters
-                  </li>
-              </ul>
-          </div>
-          <div className="avatar-box">
-               <img src='' alt='Profile image' />
-          </div>
-          </div>
+            <div className='nav-1'>
+           <div className='md-col-1 brand-box'>
+             <a href='#' className=''>
+                 Dantata
+             </a>
+           </div>
+
+        <div className='md-col-10 collabsible-box bg-green'>
+           <div className='col-10 menu-box bg-dark'>
+             <ul className='menu-list'>
+             <li>
+                 Solar panels
+             </li>
+
+             <li>
+                 Solar inverters
+             </li>
+
+             <li>
+                 Automatic changeover
+             </li>
+             </ul>
+           </div>
         </div>
+        </div>
+        <div className='nav-2'>
+          <div className='md-col-1'>
+          <button className='catalogue-btn'>Catalogue</button>
+          </div>
+          <div className='md-col-9'>
+          <input className='navbar-input' placeholder='I am looking for...'/>
+          </div>
+           <div className='md-col-2 bg-black'>
+             <img src='/assets/images/dog.jpg' className='profile-avatar' alt='Profile image'/>
+             <span className=''>
+               Ogunmola Samuel
+             </span>
+           </div>
+           </div>
+    </div>
     )
 }
 
